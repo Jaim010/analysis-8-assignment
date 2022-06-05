c3e060026878d561f71da308aff550271b0eaefe
Jamey <jameyschaap06@gmail.com>
2022-06-05 21:19:18 +0200

    Fixed unit testing with sample data

--- tests/test_database_UT.py
+++ tests/test_database_UT.py
@@ -1,37 +1,18 @@
+import os
 import unittest
 from utils import database
 
 class TestDatabase(unittest.TestCase):
-  def test_sql_injections(self):
-    db_setup()
-    
-    database.execute_query("INSERT INTO users (username) VALUES ('admin')")
-    
-    with open("./tests/test_cases.txt", "r") as f:
-      test_cases = f.readlines()
-
-    for test in test_cases:
-      result = database.execute_query("SELECT * FROM users WHERE username=?", test)
-      self.assertEqual(0, len(result))
-      
-    database.execute_query("DELETE FROM users WHERE username='admin'")
-      
   def test_execute_query_given_simple_username(self):
-    db_setup()
     username = "admin"
-    
-    database.execute_query("INSERT INTO users (username) VALUES (?)", username)
     
     result = database.execute_query("SELECT username FROM users WHERE username=?", username)
     
     self.assertEqual(1, len(result))
     self.assertEqual(username, result[0][0])
     
-    database.execute_query("DELETE FROM users WHERE username=?", username)
-    
     
   def test_execute_query_given_complex_username(self):
-    db_setup()
     username = "John O'Conner"
     
     database.execute_query("INSERT INTO users (username) VALUES (?)", username)
@@ -43,8 +24,36 @@ class TestDatabase(unittest.TestCase):
     database.execute_query("DELETE FROM users WHERE username=?", username)
     
 
-def db_setup():
-  db_path = "./tests/test.sqlite"
-  
-  database.create_connection(db_path)
-  database.setup()
+def create_test_func(query_param):
+  def test_sql_injections(self):
+    with open("./tests/test_cases.txt", "r") as f:
+      test_cases = f.readlines()
+
+    for test in test_cases:
+      result = database.execute_query("SELECT * FROM users WHERE username=?", query_param)
+      self.assertEqual(0, len(result))
+  return test_sql_injections
+
+
+# Reads test cases
+with open("./tests/test_cases.txt", "r") as f:
+  test_cases = f.readlines()
+
+# Adds test case functions to the TestDatabase class
+for i, (case) in enumerate(test_cases):
+  setattr(TestDatabase, 'test_data_%d'%i, create_test_func(case))
+
+
+# Prepare a fresh database for testing
+db_path = "./tests/test.sqlite"
+
+if os.path.exists(db_path):
+  os.remove(db_path)
+
+database.create_connection(db_path)
+database.setup()
+
+database.execute_query("DELETE FROM users WHERE username='admin'")
+result = database.execute_query("SELECT username FROM users WHERE username=?", "admin")
+if (len(result) == 0):
+  database.execute_query("INSERT INTO users (username) VALUES (?)", "admin")
