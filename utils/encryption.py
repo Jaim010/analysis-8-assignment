9bff99fff0652e9a2e4abf6de04ff3ffc475f2ae
Jamey <jameyschaap06@gmail.com>
2022-06-05 14:28:33 +0200

    Added argsparse to encryption.py

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,4 +1,19 @@
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+  "--command",
+  "-c",
+  help="command to run on given string",
+)
+
+parser.add_argument(
+  "text",
+  type=str,
+  help="Text to perform the command over",
+)
 
 with open("./settings.json") as file:
   __SETTINGS = json.load(file)
@@ -26,36 +41,22 @@ def decrypt(text: str) -> str:
   return __ceasar_cipher(__SUBTRACT)(text)
 
 
-if __name__ == "__main__":
-  """
-  Windows: 
-    python ./utils/encryption.py [-d] <INPUT_STRING>
-    python ./utils/encryption.py [-e] <INPUT_STRING>
-    
-    Example:
-      python ./utils/encryption.py -e HelloWorld!
-    
-  Unix:
-    python3 ./utils/encryption.py [-d] <INPUT_STRING>
-    python3 ./utils/encryption.py [-e] <INPUT_STRING>
-    
-    Example:
-      python ./utils/encryption.py -d q^È^Ê^Ò^ÖZYXH
-  """
-  import sys
-  if len(sys.argv) > 3:
-    raise f"Too many arguments. Expected 2, got {len(sys.argv)}" 
+
+def main(args):
+  actions = {
+    "encrypt": encrypt,
+    "decrypt": decrypt
+  }
   
-  action: callable
+  if args.command not in actions.keys():
+    raise Exception(f"No such command exists: {args.command}")
   
-  if sys.argv[1] == "-d":
-    action = decrypt
-  elif sys.argv[1] == "-e":
-    action = encrypt
-  else:
-    raise f"Invalid flag"  
-  
-  result = action(sys.argv[2])
+  result = actions[args.command](args.text)
   print(result)
+
+if __name__ == "__main__":
+  main(parser.parse_args())  
+
+  
   
   
--- utils/user.py
+++ utils/user.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 
 @dataclass
-class User():
+class User:
   first_name: str 
   last_name: str 
   username: str 
