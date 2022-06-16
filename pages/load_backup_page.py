a26413a4e6b201529810ba30c8a54d716a9bd6a4
Jamey <jameyschaap06@gmail.com>
2022-06-16 17:26:07 +0200

    Minor ui update in loadbackuppage

--- pages/load_backup_page.py
+++ pages/load_backup_page.py
@@ -22,53 +22,58 @@ class LoadBackupPage(Page):
     values = listdir("./backups/")
     keys = [i for i in range(1, len(values)+1)]
     mapping = dict(zip(keys, values))
+    
+    if len(values) > 0:
+      for key, value in mapping.items():
+        posix_time_stamp = re.search("backup-(.*).zip", value).group(1)
+        posix_time_stamp = int(posix_time_stamp)
+        time_stamp = datetime.fromtimestamp(posix_time_stamp)
+        print(
+          f"ID: {key}\n" + 
+          f"> Backup created on: {time_stamp}\n" + 
+          f"> File: {value}\n"
+        )
+        
+      print("Enter backup ID:")
+      user_input = input("> ")
+      
+      if not user_input.isnumeric():
+        self.error = "# Invalid input. Input has to be a number. #"
+        logger.log(
+          activity=self.activity,
+          information=f"Invalid input, user entered: {user_input}",
+          user=self.controller.user
+        )
+        return
+      
+      user_input = int(user_input)
+      
+      if user_input > keys[-1]:
+        self.error = "# Invalid input. Input does not map to a backup. #"
+        logger.log(
+          activity=self.activity,
+          information=f"Invalid input, user entered: {user_input}",
+          user=self.controller.user
+        )
+        return
+      
+      self.cls()
+      try:
+        with ZipFile(f"./backups/{mapping[user_input]}", "r") as zipObj:
+          zipObj.extractall("./")
+        print("Backup has succesfully been loaded.\n")
 
-    for key, value in mapping.items():
-      posix_time_stamp = re.search("backup-(.*).zip", value).group(1)
-      posix_time_stamp = int(posix_time_stamp)
-      time_stamp = datetime.fromtimestamp(posix_time_stamp)
-      print(
-        f"ID: {key}\n" + 
-        f"> Backup created on: {time_stamp}\n" + 
-        f"> File: {value}\n"
-      )
-      
-    print("Enter backup ID:")
-    user_input = input("> ")
+        logger_info = f"Restored from backup: {mapping[user_input]}"
+        
+      except Exception as err:
+        print(f"Restoring from backup failed.")
+        print(f"Error: {err}")
+        
+        logger_info = f"Failed to restore from backup. Error: {err}"
     
-    if not user_input.isnumeric():
-      self.error = "# Invalid input. Input has to be a number. #"
-      logger.log(
-        activity=self.activity,
-        information=f"Invalid input, user entered: {user_input}",
-        user=self.controller.user
-      )
-      return
-    
-    user_input = int(user_input)
-    
-    if user_input > keys[-1]:
-      self.error = "# Invalid input. Input does not map to a backup. #"
-      logger.log(
-        activity=self.activity,
-        information=f"Invalid input, user entered: {user_input}",
-        user=self.controller.user
-      )
-      return
-    
-    self.cls()
-    try:
-      with ZipFile(f"./backups/{mapping[user_input]}", "r") as zipObj:
-        zipObj.extractall("./")
-      print("Backup has succesfully been loaded.\n")
-
-      logger_info = f"Restored from backup: {mapping[user_input]}",
-      
-    except Exception as err:
-      print(f"Restoring from backup failed.")
-      print(f"Error: {err}")
-      
-      logger_info = f"Failed to restore from backup. Error: {err}",
+    else:
+      print("There are no backups.\n")
+      logger_info = "No backups available"  
       
     logger.log(
       activity=self.activity,
